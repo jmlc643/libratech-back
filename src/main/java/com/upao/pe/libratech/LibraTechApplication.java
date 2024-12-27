362dde22cdfb01bd24e2f4514dfe0bc81b973c98
jmlc643 <josemarialuyocampos@gmail.com>
2024-12-26 23:26:37 -0500

    First commit

---
+++ src/main/java/com/upao/pe/libratech/LibraTechApplication.java
@@ -0,0 +1,13 @@
+package com.upao.pe.libratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraTechApplication.class, args);
+    }
+
+}
